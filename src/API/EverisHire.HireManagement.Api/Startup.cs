d2522e6413675689c5c3e2140d1000b77c164359
MG7 <matheus.campos@edu.unifil.br>
2021-05-22 15:53:35 -0300

    feat: api configuration

---
+++ src/API/EverisHire.HireManagement.Api/Startup.cs
@@ -0,0 +1,77 @@
+
+using EverisHire.HireManagement.Api.Middleware;
+using EverisHire.HireManagement.Application;
+using EverisHire.HireManagement.Identity;
+using EverisHire.HireManagement.Infrastructure;
+using EverisHire.HireManagement.Persistence;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Hosting;
+
+
+namespace EverisHire.HireManagement.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddApplicationServices();
+            services.AddInfrastructureServices(Configuration);
+            services.AddPersistenceServices(Configuration);   
+            services.AddIdentityServices(Configuration);    
+            services.TryAddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+                
+            services.AddControllers();
+            services.AddCors(options => {
+                options.AddPolicy("Open", builder => 
+                builder.AllowAnyOrigin()
+                .AllowAnyHeader()
+                .AllowAnyMethod());
+            });
+            // services.AddSwaggerGen(c =>
+            // {
+            //     c.SwaggerDoc("v1", new OpenApiInfo { Title = "EverisHire.HireManagement.Api", Version = "v1" });
+            // });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                // app.UseSwagger();
+                // app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "EverisHire.HireManagement.Api v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+            app.UseAuthentication();
+
+            app.UseCustomExceptionHandler();
+
+            app.UseCors("Open");
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
